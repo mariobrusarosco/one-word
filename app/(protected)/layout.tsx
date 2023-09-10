e1bd895003723d5b0e72b01647a68279307602ac
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-09-10 11:54:31 -0300

    feat: prisma quick start

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -8,15 +8,19 @@ import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 const ProtectedLayout = async ({ children }: Props) => {
+  // const member = await MemberQueries.fetchMember();
+
+  // if (member === null) return redirect(AuthRoutes.REGISTER);
+
   return (
     <div className="grid grid-cols-12 h-full">
-      {/* <Suspense fallback={<h2>Loading...</h2>}> */}
-      <AppSidebar />
-      <TableSidebar />
-      <main className="container col-span-8 lg:col-span-9 bg-red-200">
-        {children}
-      </main>
-      {/* </Suspense> */}
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <AppSidebar />
+        <TableSidebar />
+        <main className="container col-span-8 lg:col-span-9 bg-red-200">
+          {children}
+        </main>
+      </Suspense>
     </div>
   );
 };

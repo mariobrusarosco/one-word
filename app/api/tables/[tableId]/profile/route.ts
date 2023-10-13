527f92a6e76569ac22b4ec57e95c5ce8b12a161a
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-10-12 21:31:51 -0300

    fix(Table): updating profile

--- app/api/tables/[tableId]/profile/route.ts
+++ app/api/tables/[tableId]/profile/route.ts
@@ -35,8 +35,17 @@ export async function PATCH(
 
     const payload = (await req.json()) as UpdateProfileRole;
 
+    const tableProfile = await db.tableProfile.findFirst({
+      where: {
+        memberId: payload.profileId,
+      },
+    });
+    if (!tableProfile) return false;
+
     const updateProfile = await db.tableProfile.update({
-      where: { memberId: payload.profileId },
+      where: {
+        id: tableProfile.id,
+      },
       data: {
         role: payload.role,
       },

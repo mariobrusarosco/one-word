94ebc02143141e490c883ff96d9074b02b29ff74
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-10-28 14:50:15 -0300

    [main][Learning] - chore(Setup): MSW setup

--- main/src/main.tsx
+++ main/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const enableApiMocking = async () => {
+  if (process.env.NODE_ENV !== "development") return;
+
+  const { worker } = await import("./mocks/browser");
+
+  await worker.start();
+};
+
+enableApiMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});

81568254c961c67e40c81d796428f40a8307131d
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-11-04 11:14:43 -0300

    [main][Learning]: msw - using QUery/Search Parameters

--- main/src/mocks/handlers.ts
+++ main/src/mocks/handlers.ts
@@ -1,9 +1,9 @@
 import { http, HttpResponse } from "msw";
 
 export const handlers = [
-  http.get(`${import.meta.env.VITE_BASE_API_URL}/tables`, () => {
-    return HttpResponse.json([{ name: "Skol" }]);
-  }),
+  // http.get(`${import.meta.env.VITE_BASE_API_URL}/tables`, () => {
+  //   return HttpResponse.json([{ name: "Skol" }]);
+  // }),
 
   http.get(
     `${import.meta.env.VITE_BASE_API_URL}/tables/:tableId`,
@@ -13,4 +13,11 @@ export const handlers = [
       return HttpResponse.json([{ name: tableId }]);
     }
   ),
+
+  http.get(`${import.meta.env.VITE_BASE_API_URL}/tables`, ({ request }) => {
+    const url = new URL(request.url);
+    const activeGame = url.searchParams.get("active_game");
+
+    return HttpResponse.json([{ name: "skol", active_game: activeGame }]);
+  }),
 ];

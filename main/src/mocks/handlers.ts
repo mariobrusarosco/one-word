9eae1fdde6bb57f64fd93d00d3a1367da20b1fdd
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-11-04 11:02:12 -0300

    [main][Learning]: msw - using Dynamic Parameters

--- main/src/mocks/handlers.ts
+++ main/src/mocks/handlers.ts
@@ -1,6 +1,16 @@
 import { http, HttpResponse } from "msw";
+
 export const handlers = [
   http.get(`${import.meta.env.VITE_BASE_API_URL}/tables`, () => {
     return HttpResponse.json([{ name: "Skol" }]);
   }),
+
+  http.get(
+    `${import.meta.env.VITE_BASE_API_URL}/tables/:tableId`,
+    ({ params }) => {
+      const tableId = params.tableId;
+
+      return HttpResponse.json([{ name: tableId }]);
+    }
+  ),
 ];

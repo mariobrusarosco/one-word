9d5a5a45b627bdcfc4412e59f184adf76599d3a9
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-12-19 11:37:20 -0300

    housekeeping: creating a Testing component

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -3,8 +3,15 @@ import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Games } from "./domains/games/components/Games";
 import { Tables } from "./domains/tables/components/Tables";
+import { Testing } from "./domains/shared/testing-temp/testing";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+  },
+});
 
 const AppWithProviders = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -17,7 +24,7 @@ function App() {
     <AppWithProviders>
       <Tables />
       <Games />
-      {/* <Testing /> */}
+      <Testing />
     </AppWithProviders>
   );
 }

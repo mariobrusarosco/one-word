ad59dc9fe15ae63fd8b7cbc69f69180d7f2559eb
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-12-19 09:49:44 -0300

    [LEARNING]/[ROUTING] - Active Links / Navigation list of Links

--- main/src/domains/auth/components/authenticated-layout.tsx
+++ main/src/domains/auth/components/authenticated-layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import { NavigationBar } from "../../shared/components/navigation-bar";
 
 export const AuthenticatedLayout = () => {
   return (
     <div style={{ border: "1px solid orange" }}>
       <h1>AuthenticatedLayout</h1>
+      <NavigationBar />
       <Outlet />
     </div>
   );

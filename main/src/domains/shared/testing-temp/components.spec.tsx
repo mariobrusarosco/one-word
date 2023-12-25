6dec2c2eb5797d126f11bc5263507029d638d4d7
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-12-25 15:47:33 -0300

    [LEARNING/TESTING] - Test a Component that has performed a click event

--- main/src/domains/shared/testing-temp/components.spec.tsx
+++ main/src/domains/shared/testing-temp/components.spec.tsx
@@ -60,3 +60,15 @@ describe("[UNIT] - DynamicListGames", () => {
     });
   });
 });
+
+describe("[UNIT] - Toggle", () => {
+  describe("when clicking and the content is hidden", () => {
+    it("displays the content", async () => {
+      const { user } = setupAndRender(<Toggle />);
+
+      await user.click(screen.getByText(/toggle/i));
+
+      expect(screen.getByText(/hello!/i)).toBeInTheDocument();
+    });
+  });
+});

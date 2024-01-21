0d75a3baa776b0c78af27aca33c18b2a2284df82
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-01-21 10:48:39 -0300

    feat(Main): Adding Tailwind Support

--- main/src/main.tsx
+++ main/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { setApiMockingWhenInDevMode } from "./domains/shared/utils/dev-api-mocking.ts";
 
 const runWebApp = async () => {

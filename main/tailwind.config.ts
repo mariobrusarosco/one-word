3d22198ce4cbf473833790fe29542d6dbb2858bf
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-04-08 11:27:21 -0300

    [STYLES - SETUP]: Setting App's color tokens

--- main/tailwind.config.ts
+++ main/tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 import { APP_CONTAINER } from "./src/styling/constants/app-container";
 import { APP_FONTS } from "./src/styling/constants/fonts";
 import { APP_BREAKPOINTS } from "./src/styling/constants/breakpoints-and-media-queries";
+import { ONE_WORD_PALETTE } from "./src/styling/constants/palette";
 
 const ONE_WORD_TAILWIND_CONFIG: Config = {
   content: ["./src/**/*.{ts,tsx}"],
@@ -11,6 +12,7 @@ const ONE_WORD_TAILWIND_CONFIG: Config = {
       fontFamily: APP_FONTS.fontFamily,
     },
     container: APP_CONTAINER,
+    colors: ONE_WORD_PALETTE,
   },
 };
 

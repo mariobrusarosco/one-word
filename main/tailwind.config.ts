744371e0fc0fc896e5b7cb568336ebbf144b602b
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-04-08 14:38:41 -0300

    [STYLING - CHADCN - SETUP]: Installing SHADCN with CSS Variables and Tailwind

--- main/tailwind.config.ts
+++ main/tailwind.config.ts
@@ -3,17 +3,42 @@ import { APP_CONTAINER } from "./src/styling/constants/app-container";
 import { APP_FONTS } from "./src/styling/constants/fonts";
 import { APP_BREAKPOINTS } from "./src/styling/constants/breakpoints-and-media-queries";
 import { ONE_WORD_PALETTE } from "./src/styling/constants/palette";
+import tailwindcssAnimatePlugin from "tailwindcss-animate";
 
-const ONE_WORD_TAILWIND_CONFIG: Config = {
+const config = {
+  darkMode: ["class"],
   content: ["./src/**/*.{ts,tsx}"],
+  prefix: "",
   theme: {
     screens: APP_BREAKPOINTS,
+    container: APP_CONTAINER,
     extend: {
       fontFamily: APP_FONTS.fontFamily,
+      colors: {
+        ...ONE_WORD_PALETTE,
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
-    container: APP_CONTAINER,
-    colors: ONE_WORD_PALETTE,
   },
-};
+  plugins: [tailwindcssAnimatePlugin],
+} satisfies Config;
 
-export default ONE_WORD_TAILWIND_CONFIG;
+export default config;

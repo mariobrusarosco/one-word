f4721d48131cf1cb0a43ed6e1dab3574b099a386
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-04-08 10:52:42 -0300

    [STYLES - SETUP]: Setting App's main container element

--- main/project-docs/styling.md
+++ main/project-docs/styling.md
@@ -4,3 +4,8 @@
 
 This app will validate the usage of Custom breakpoints instead
 of using Tailwind's.
+
+## Page Container
+
+This app will use a custom size for its main "Container" DOM Element.
+Setup was made via `tailwind.config`
--- main/tailwind.config.ts
+++ main/tailwind.config.ts
@@ -1,19 +1,10 @@
 import type { Config } from "tailwindcss";
+import { APP_CONTAINER } from "./src/styling/constants/app-container";
 
 const ONE_WORD_TAILWIND_CONFIG: Config = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    screens: {
-      "m-only": { max: "767px" },
-      tablet: { min: "768px" },
-      desktop: { min: "1280px" },
-    },
-    extend: {
-      fontFamily: {
-        serif: ["Roboto Slab", "serif"],
-        sans: ["Roboto", "sans-serif"],
-      },
-    },
+    container: APP_CONTAINER,
   },
 };
 

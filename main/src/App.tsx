3da5694c6eb8f0d7e478089a2731ed308ec33465
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-05-21 15:49:52 -0300

    fix: rollback on app.tsx renaming

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,11 +1,9 @@
-import "./app.css";
+import "./App.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AppRouter } from "./domains/shared/components/app-router";
 import { useAppTheme } from "./domains/ui-system/theming/theme";
-
-// import { useTheme } from "./domains/ui-system/theming/theme-provider";
 
 const queryClient = new QueryClient({
   defaultOptions: {

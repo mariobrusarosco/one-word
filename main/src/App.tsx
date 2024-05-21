3281215f0b1fe8f139149c8ca55eac1862831e17
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-05-21 15:44:07 -0300

    fix: wrong import for app.css

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./app.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";

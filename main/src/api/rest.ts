fff840e51697e1cecc730be558a6be41a5470b6c
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-06-02 17:01:13 -0300

    feat: sending cookies via axios

--- main/src/api/rest.ts
+++ main/src/api/rest.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
+// axios.defaults.withCredentials = true;
 export const restApi = axios.create({
   baseURL: import.meta.env.VITE_ONE_WORD_API,
-  // withCredentials: true,
+  withCredentials: true,
 });

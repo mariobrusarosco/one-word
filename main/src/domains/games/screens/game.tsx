7d4296e290617bec95bf08fecfc47c79dcbda3a3
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-06-06 09:12:49 -0300

    feat: intro page

--- main/src/domains/games/screens/game.tsx
+++ main/src/domains/games/screens/game.tsx
@@ -1,11 +1,23 @@
 import { useTableSocketManager } from "@/domains/tables/provider";
+import { Separator } from "@/domains/ui-system/components/ui/separator";
 
 const GameScreen = () => {
   const tableSocket = useTableSocketManager();
 
   return (
-    <div>
-      <h2>Game</h2>
+    <div data-ui="game-screen">
+      <div className="heading font-extralight">
+        <div className="flex justify-between items-center font-sans ">
+          <p className="text-pink-500 dark:text-teal-800 text-3xl desktop:text-5xl">
+            Game
+          </p>
+          <p className="table-name font-sans text-3xl text-teal-800 dark:text-white-100 desktop:text-5xl">
+            Not Started
+          </p>
+        </div>
+
+        <Separator className="bg-pink-500 my-4" />
+      </div>
 
       <ul className="grid overflow-auto h-full pb-10">
         {tableSocket.participants.map((participant) => (
